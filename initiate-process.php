8b003dd21e431548d5eb420b76a6ba76550f51b7
Ethan Gruber <ewg4xuva@gmail.com>
2026-01-13 13:00:42 -0500

    changed to prod

--- initiate-process.php
+++ initiate-process.php
@@ -27,7 +27,7 @@ $projectExists = false;
 
 
 //$mode should be 'test' or 'prod', which determines whether the NUDS/XML is written to the console or to eXist-db and indexed into Numishare
-$mode = 'test';
+$mode = 'prod';
 
 $nomismaUris = array();
 $errors = array();
